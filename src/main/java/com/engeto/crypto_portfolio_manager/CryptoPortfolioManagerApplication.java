838b8faf3c78de731b288def289b2a440dfed2bf
Tomáš Weber <tomas.weber@email.cz>
2025-03-22 22:29:56 +0100

    minor update - code moved to same line

--- src/main/java/com/engeto/crypto_portfolio_manager/CryptoPortfolioManagerApplication.java
+++ src/main/java/com/engeto/crypto_portfolio_manager/CryptoPortfolioManagerApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class
-CryptoPortfolioManagerApplication {
+public class CryptoPortfolioManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoPortfolioManagerApplication.class, args);
